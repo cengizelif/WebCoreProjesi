3fb7757ab7cd2f2cf357f690d002d985934d229b
cengizelif <cenelif@gmail.com>
2023-05-25 11:23:57 +0300

    Add project files.

---
+++ WebCoreProjesi/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebCoreProjesi.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
